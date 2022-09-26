93ae3194533abcd59902814ecd6cfa6995e183f2
kooshal <kooshalkashi7@gmail.com>
2022-09-26 18:03:23 +0400

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-import path from "path";
-import express from "express";
-import dotenv from "dotenv";
-import colors from "colors";
-import morgan from "morgan";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import connectDB from "./config/db.js";
-import patientRoutes from "./routes/patientRoutes.js";
-import appointmentRoutes from "./routes/appointmentRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import bookRoutes from "./routes/bookRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import morgan from 'morgan';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+import patientRoutes from './routes/patientRoutes.js';
+import appointmentRoutes from './routes/appointmentRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import bookRoutes from './routes/bookRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -17,34 +17,34 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
-app.use("/api/appointments", appointmentRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/books", bookRoutes);
-app.use("/api/upload", uploadRoutes);
-app.use("/patient", patientRoutes);
+app.use('/api/appointments', appointmentRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/books', bookRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/patient', patientRoutes);
 
-app.get("/api/config/paypal", (req, res) =>
+app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === "appointmention") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
+  app.get('/', (req, res) => {
+    res.send('API is running....');
   });
 }
 
